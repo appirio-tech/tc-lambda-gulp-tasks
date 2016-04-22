42b2c8fccc5733ce09ff093badc2241f704f62da
Parth Shah <parth@topcoder.com>
2016-04-22 11:19:31 -0700

    reading cofnig from package.json

--- index.js
+++ index.js
@@ -1,37 +1,57 @@
 var gulp = require('gulp'),
-  zip = require('gulp-zip'),
-  del = require('del'),
-  install = require('gulp-install'),
-  awsLambda = require('node-aws-lambda'),
-  envify = require('gulp-envify'),
-  path = require('path');
+    zip = require('gulp-zip'),
+    del = require('del'),
+    install = require('gulp-install'),
+    awsLambda = require('node-aws-lambda'),
+    envify = require('gulp-envify'),
+    _ = require('lodash'),
+    path = require('path');
 
 module.exports = function(gulp) {
 
-  gulp.task('clean', function(cb) {
-    return del(['./dist', './dist.zip'], cb);
-  });
+    gulp.task('clean', function(cb) {
+        return del(['./dist', './dist.zip'], cb);
+    });
 
-  gulp.task('js', function() {
-    return gulp.src(['src/**/*', '!src/**/*.spec.js'])
-      .pipe(envify(process.env))
-      .pipe(gulp.dest('dist/'));
-  });
+    gulp.task('js', function() {
+        return gulp.src(['src/**/*', '!src/**/*.spec.js'])
+            .pipe(envify(process.env))
+            .pipe(gulp.dest('dist/'));
+    });
 
-  gulp.task('node-mods', function() {
-    return gulp.src('./package.json')
-      .pipe(gulp.dest('dist/'))
-      .pipe(install({ production: true }));
-  });
+    gulp.task('node-mods', function() {
+        return gulp.src('./package.json')
+            .pipe(gulp.dest('dist/'))
+            .pipe(install({
+                production: true
+            }));
+    });
 
-  gulp.task('zip', function() {
-    return gulp.src(['dist/**/*', '!dist/package.json'])
-      .pipe(zip('dist.zip'))
-      .pipe(gulp.dest('./'));
-  });
+    gulp.task('zip', function() {
+        return gulp.src(['dist/**/*', '!dist/package.json'])
+            .pipe(zip('dist.zip'))
+            .pipe(gulp.dest('./'));
+    });
 
-  gulp.task('upload', function(callback) {
-    awsLambda.deploy('./dist.zip', require(path.join(process.cwd(), "lambda-config.js")), callback);
-  });
+    gulp.task('upload', function(callback) {
+        var packageDefinition = require(path.join(process.cwd(), './package.json'))
+
+        var lambdaConfig = packageDefinition.lambda;
+        // set defaults
+        _.defaultsDeep(lambdaConfig, {
+            region: 'us-east-1',
+            handler: 'index.handler',
+            description: "",
+            role: 'arn:aws:iam::' + process.env.AWS_ACCOUNT_ID + ':role/' + (packageDefinition.lambda.role || 'aws-lambda-default'),
+            timeout: 180,
+            memorySize: 512
+        });
+
+        awsLambda.deploy(
+            './dist.zip', lambdaConfig,
+            callback);
+    });
+
+
 
 };
